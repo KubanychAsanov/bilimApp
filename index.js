6bb67a64d5ede17e25740715958f171e73996276
Kubanych Asanov <kuba.asanov4@gmail.com>
2021-05-06 11:45:15 +0600

    connected to mongoDB

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const path = require('path')
+const scrf = require('csurf')
+const flash = require('connect-flash')
+const mongoose = require('mongoose')
+const session = require('express-session')
+const MongoStote = require('connect-mongodb-session')(session)
+var Handlebars = require('handlebars')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const cardRoutes = require('./routes/card')
 const addRoutes = require('./routes/add')
+const profileRoutes = require('./routes/profile')
 const coursesRoutes = require('./routes/courses')
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const authRoutes = require('./routes/auth')
+const User = require('./models/user')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
 
+const MONGODB_URI = "mongodb+srv://kuba:HZNatTQKIZIspRIL@cluster0.hjuuw.mongodb.net/blog";
 const app = express()
-
 const hbs = exphbs.create({
   defaultLayout: 'main',
-  extname: 'hbs'
+  extname: 'hbs',
+  helpers: require('./utils/hbs-helpers'),
+  handlebars: allowInsecurePrototypeAccess(Handlebars)
+})
+const store = new MongoStote({
+  collection: 'sessions',
+  uri: MONGODB_URI
 })
 
 app.engine('hbs', hbs.engine)
@@ -18,15 +36,43 @@ app.set('view engine', 'hbs')
 app.set('views', 'views')
 
 app.use(express.static(path.join(__dirname, 'public')))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: 'some secret value',
+  resave: false,
+  saveUninitialized: false,
+  store
+}))
+app.use(scrf())
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
 
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/card', cardRoutes)
+app.use('/auth', authRoutes)
+app.use('/profile', profileRoutes)
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+async function start() {
+  try {
+    await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    })
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`)
+    })
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
+
--- public/app.js
+++ public/app.js
@@ -1,22 +1,15 @@
-const toCurrency = price => {
-  return new Intl.NumberFormat('ru-RU', {
-    currency: 'rub',
-    style: 'currency'
-  }).format(price)
-}
-
-document.querySelectorAll('.price').forEach(node => {
-  node.textContent = toCurrency(node.textContent)
-})
-
 const $card = document.querySelector('#card')
 if ($card) {
   $card.addEventListener('click', event => {
     if (event.target.classList.contains('js-remove')) {
       const id = event.target.dataset.id
+      const csrf = event.target.dataset.csrf
       
       fetch('/card/remove/' + id, {
-        method: 'delete'
+        method: 'delete',
+        headers: {
+          'X-XSRF-TOKEN': csrf
+        }
       }).then(res => res.json())
         .then(card => {
           if (card.courses.length) {
@@ -41,3 +34,4 @@ if ($card) {
     
   })
 } 
+M.Tabs.init(document.querySelectorAll('.tabs'))
